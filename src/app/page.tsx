fcea83f8bc080fca499c8131217faa02a4b4eb5e
Alvaro Llorente <allorentee@gmail.com>
2025-09-13 12:02:15 +0200

    improve: change texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import EmailForm from "@/components/email-form";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Demo send emails with Next.js and MailerSend</h1>
+      <h1>Demo send emails with Next.js and NodeMailer</h1>
       <p className="text-center">
-        This is a demo of how to send emails with Next.js and MailerSend
+        This is a demo of how to send emails with Next.js and NodeMailer
       </p>
       <EmailForm />
     </div>
